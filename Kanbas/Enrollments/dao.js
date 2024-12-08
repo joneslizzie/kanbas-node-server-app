d59ee7939af7859f97a2f240eba4d554fa368bdd
Lizzie <jonesmlizzie@gmail.com>
2024-12-08 16:51:47 -0500

    added people

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -9,3 +9,7 @@ export function enrollUserInCourse(user, course) {
  export function unenrollUserFromCourse(user, course) {
   return model.deleteOne({ user, course });
  }
+ export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
+}
